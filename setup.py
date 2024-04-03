4aad1d2c67c5c3919c952c59c3535844b2b315cf
Vinoth <vinothaj0619@gmail.com>
2024-04-03 18:38:46 +0530

    ML_project info added

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->list[str]:
-    # This finction will return the list of requirements
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements 
+    '''
 
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n","")for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
     return requirements
 
-set(
-name = 'mlproject',
-version = '0.0.1',
-author = 'krish',
-author_email = 'krishnaik06@gmail.com',
-packages = find_packages(),
-install_requires =  get_requirements('requirements.txt')
+setup(
+        name = 'ML Project',
+        version = '0.0.1',
+        author = 'vinoth',
+        author_email = 'vinothaj0619@gmail.com',
+        packages = find_packages(),
+        install_requires = get_requirements('requirements.txt')
 )
